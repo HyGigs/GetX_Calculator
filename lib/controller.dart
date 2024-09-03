e4ee64b2a0844e3d3469288c56010314fc940072
azmx <muhammadazzamaqiladafffa@gmail.com>
2024-09-03 19:23:14 +0700

    feat: [AZ] aritmatic

--- lib/controller.dart
+++ lib/controller.dart
@@ -1,3 +1,4 @@
+import 'package:calculator_getx/pages/kalkulator_aritmatika.dart';
 import 'package:get/get.dart';
 import 'package:calculator_getx/pages/keliling_persegi_panjang.dart';
 import 'package:calculator_getx/pages/keliling_segitiga.dart';
@@ -5,6 +6,11 @@ import 'package:calculator_getx/pages/keliling_segitiga.dart';
 class Controller extends GetxController {
   final hasilKelilingSegitiga = RxDouble(0.0);
   final hasilKelilingPersegiPanjang = RxDouble(0.0);
+  final hasilPertambahan = RxDouble(0.0);
+  final hasilPengurangan = RxDouble(0.0);
+  final hasilPerkalian = RxDouble(0.0);
+  final hasilPembagian = RxDouble(0.0);
+  final hasilModulus = RxDouble(0.0);
 
   KelilingSegitiga (double alas,double tinggi){
     hasilKelilingSegitiga.value = 0.5 * alas * tinggi;
@@ -12,6 +18,12 @@ class Controller extends GetxController {
   kelilingPersegiPanjang(double panjang, double lebar){
     hasilKelilingPersegiPanjang.value = 2*panjang * lebar;
 }
-
+  rumusAritmatika(double angka1, double angka2){
+    hasilPertambahan.value = angka1 + angka2;
+    hasilModulus.value = angka1 % angka2;
+    hasilPembagian.value = angka1 / angka2;
+    hasilPengurangan.value = angka1 - angka2;
+    hasilPerkalian.value = angka1 * angka2;
+  }
 }
 
